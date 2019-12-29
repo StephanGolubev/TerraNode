ff95ac6d71c2602c1b7d391347ca355ca28d8269
ase21 <ase911@yandex.ru>
2019-12-29 23:44:43 +0300

    доделать кружок вокруг авы, привести в порядок ID

--- app/src/main/java/com/itn/terranode/data/network/NetworkRepository.java
+++ app/src/main/java/com/itn/terranode/data/network/NetworkRepository.java
@@ -8,6 +8,7 @@ import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface NetworkRepository {
@@ -19,15 +20,19 @@ public interface NetworkRepository {
     Maybe<Response<Object>> login(@Body LoginDTO gatewayAuthorizeRequest);
 
     @GET("/api/v1/user")
+    @Headers("Accept: application/json")
     Maybe<Response<Object>> getInformationAboutUser(@Header("Authorization") String token);
 
     @GET("/api/v1/news")
+    @Headers("Accept: application/json")
     Maybe<Response<Object>> getNews(@Header("Authorization") String token);
 
     @GET("/api/v1/products")
+    @Headers("Accept: application/json")
     Maybe<Response<Object>> getProducts(@Header("Authorization") String token);
 
     @GET("/api/v1/chat")
+    @Headers("Accept: application/json")
     Maybe<Response<Object>> getChats(@Header("Authorization") String token);
 
 }
